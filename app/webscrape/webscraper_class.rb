af6f0f366a3128184b23a116d7064c2c6804be3e
SimonPringleWallace <s.pringlewallace@gmail.com>
2018-11-29 15:57:12 -0500

    Create Working Pagination
    - Successfully iterate through all of the pages required (13 of them)
      and print the needed information from every page to the console.
    - Accomplish this through a while loop in the Scraper class that
      increments that page number upwards until it reaches 12

--- app/webscrape/webscraper_class.rb
+++ app/webscrape/webscraper_class.rb
@@ -3,14 +3,16 @@
 require 'HTTParty'
 require 'Nokogiri'
 require 'json'
+BASE_URL= 'https://www.gunviolencearchive.org'
+BASE_DIR ='/reports/mass-shooting?page'
 
 # Scraper class to provide webscraping capabilities
 class Scraper
   attr_accessor :parse_data
 
-  def initialize
+  def initialize(pageNum)
     # get the raw data from the website using the HTTParty get request
-    raw_data = HTTParty.get('https://www.gunviolencearchive.org/reports/mass-shooting')
+    raw_data = HTTParty.get(BASE_URL + BASE_DIR + "=#{pageNum}")
     @parse_data ||= Nokogiri::HTML(raw_data)
   end
 
@@ -19,29 +21,34 @@ class Scraper
   end
 
   # create new instance of scraper
-  scraper = Scraper.new
-  # scrape and place response into an array
-  all_incident_info = scraper.scrape_incident_data
-  incident_array = []
-  # slice off 9 indexes of all_incident_info at a time (equates to one row of
-  # information on the scraped site)
-  while all_incident_info.empty? == false
-    incident = all_incident_info.slice!(0,8)
-    # remove empty index from the end of each selection
-    incident.pop
-    # add the selection to the incident array
-    incident_array.push(incident)
-  end
-  # for each index of the incident_array create a new has with it's vales and
-  # print that to the console
-  (0...incident_array.size).each do |index|
-    puts incident_hash = {
-      'date' => incident_array[index][0],
-      'state' => incident_array[index][1],
-      'city' => incident_array[index][2],
-      'street' => incident_array[index][3],
-      'number_killed' => incident_array[index][4],
-      'number_injured' => incident_array[index][5]
-    }
+  @i = 0
+  while @i <=12
+    p @i
+    scraper = Scraper.new(@i)
+    # scrape and place response into an array
+    all_incident_info = scraper.scrape_incident_data
+    incident_array = []
+    # slice off 9 indexes of all_incident_info at a time (equates to one row of
+    # information on the scraped site)
+    while all_incident_info.empty? == false
+      incident = all_incident_info.slice!(0,8)
+      # remove empty index from the end of each selection
+      incident.pop
+      # add the selection to the incident array
+      incident_array.push(incident)
+    end
+    # for each index of the incident_array create a new has with it's vales and
+    # print that to the console
+    (0...incident_array.size).each do |index|
+      puts incident_hash = {
+        'date' => incident_array[index][0],
+        'state' => incident_array[index][1],
+        'city' => incident_array[index][2],
+        'street' => incident_array[index][3],
+        'number_killed' => incident_array[index][4],
+        'number_injured' => incident_array[index][5]
+      }
+      end
+  @i += 1
   end
 end
